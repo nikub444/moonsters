78bdcaafc8d090edcd5f7f75e77aad008d4261aa
nikub444 <magaszak12@gmail.com>
2020-04-28 19:52:58 +0200

    Updates

---
+++ precache-manifest.27d75082dddfc9dfba2e1547ff6e5401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8096849f34ea1a996c5cc903bdfe15",
+    "url": "/moonster/index.html"
+  },
+  {
+    "revision": "3ca3792f3ab99741ecb0",
+    "url": "/moonster/static/css/main.a7386d03.chunk.css"
+  },
+  {
+    "revision": "1ff3a414af6e8a91bdf2",
+    "url": "/moonster/static/js/2.6dd59b07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moonster/static/js/2.6dd59b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca3792f3ab99741ecb0",
+    "url": "/moonster/static/js/main.ffc9f77c.chunk.js"
+  },
+  {
+    "revision": "f975295438a840fb812e",
+    "url": "/moonster/static/js/runtime-main.f1cd92d8.js"
+  }
+]);
